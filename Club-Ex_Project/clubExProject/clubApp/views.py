bfeda6a60b85911eb90c62e3358f13ee991bb806
Daniel Tibbotts <dtibbotts@outlook.com>
2021-08-10 13:33:56 +1200

    Created views and templates added .gitignore
    Co-authored-by: sir306 <sir306@users.noreply.github.com>
    Co-authored-by: Tarun  Kumar <t191@mywhitecliffe.com>

--- Club-Ex_Project/clubExProject/clubApp/views.py
+++ Club-Ex_Project/clubExProject/clubApp/views.py
@@ -1,3 +1,45 @@
+from django import template
+from django.forms.forms import Form
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+
+
+# def index(request):
+#     return HttpResponse('<h1> this is index landing page</h1>')
+
+# landing page/home page
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class LoginView(TemplateView):
+    template_name = "login.html"
+
+
+class SignUpView(TemplateView):
+    template_name = "signup.html"
+
+
+class ViewVideoListView(TemplateView):
+    template_name = "videolist.html"
+
+
+class SearchResultsView(TemplateView):
+    template_name = "results.html"
+
+
+class VideoView(TemplateView):
+    template_name = "video.html"
+
+
+# view for the profile/account
+class AccountView(TemplateView):
+    template_name = "account.html"
+
+
+class StatsView(TemplateView):
+    template_name = "stats.html"
