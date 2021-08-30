0eff024385ff3290f396e3db4375ed6d3e493905
stangerp <prstanger@gmail.com>
2021-08-30 17:43:14 +1200

    fixed the urls to have the correct view name for SearchResults

--- Club-Ex_Project/clubExProject/clubApp/urls.py
+++ Club-Ex_Project/clubExProject/clubApp/urls.py
@@ -5,7 +5,7 @@ from .views import IndexView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('club/videos/', views.viewVideoList, name='videolist'),
-    path('club/searchresults/', views.searchResultsView, name='results'),
+    path('club/searchresults/', views.SearchResultsView, name='results'),
     path('club/video/<int:pk>', views.videoView, name='video'),
     path('club/stats/', views.statsView, name='stats'),   
 ]
