c8a85aab7357b297450cde1e5f5353b906a17f35
stangerp <prstanger@gmail.com>
2021-08-31 22:13:44 +1200

    fixed the search after it got broken by Login Decorators

--- Club-Ex_Project/clubExProject/clubApp/urls.py
+++ Club-Ex_Project/clubExProject/clubApp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import IndexView
+from .views import IndexView, SearchResultsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('club/videos/', views.viewVideoList, name='videolist'),
-    path('club/searchresults/', views.SearchResultsView, name='results'),
+    path('club/searchresults/', views.SearchResultsView.as_view(), name='results'),
     path('club/video/<int:pk>', views.videoView, name='video'),
     path('club/stats/', views.statsView, name='stats'),   
 ]
