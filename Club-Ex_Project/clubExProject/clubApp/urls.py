c998c711426b90170f13fd09a67a0b8b9e54efd8
PeterStanger92020362 <65217016+PeterStanger92020362@users.noreply.github.com>
2021-09-07 20:04:47 +1200

    updated urls.py for Search function to work

--- Club-Ex_Project/clubExProject/clubApp/urls.py
+++ Club-Ex_Project/clubExProject/clubApp/urls.py
@@ -5,7 +5,7 @@ from .views import IndexView
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('club/videos/', views.viewVideoList, name='videolist'),
-    path('club/searchresults/', views.SearchResultsView, name='results'),
+    path('club/searchresults/', views.SearchResultsView.as_view(), name='results'),
     path('club/video/<int:pk>', views.videoView, name='video'),
     path('club/stats/<int:pk>', views.StatisticsView.as_view(), name='stats'),      
 ]
