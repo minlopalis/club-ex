9173076d89f97329fc9708696d591b3b150bd074
minlopalis <52741304+minlopalis@users.noreply.github.com>
2021-09-08 13:08:58 +1200

    Added Customer Sum Stat

--- Club-Ex_Project/clubExProject/clubApp/templatetags/apptags.py
+++ Club-Ex_Project/clubExProject/clubApp/templatetags/apptags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Video, VideoWatchTime
+from ..models import Customer
 from django.db.models import Sum
 
 register = template.Library()
@@ -15,3 +16,7 @@ def count_videos():
 @register.simple_tag()
 def sum_watch_time():
     return VideoWatchTime.objects.aggregate(Sum('total_watch_time'))['total_watch_time__sum']
+
+@register.simple_tag()
+def count_customers():
+    return Customer.objects.count()
