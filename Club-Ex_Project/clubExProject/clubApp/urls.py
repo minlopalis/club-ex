c18d91c9ac2d2059e4499cc6c34fafbc18d35465
minlopalis <52741304+minlopalis@users.noreply.github.com>
2021-09-13 16:01:51 +1200

    Removed Sandbox from URLs

--- Club-Ex_Project/clubExProject/clubApp/urls.py
+++ Club-Ex_Project/clubExProject/clubApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('club/videos/', views.viewVideoList, name='videolist'),
     path('club/video/<int:pk>', views.videoView, name='video'),
     path('club/stats/<int:pk>', views.StatisticsView.as_view(), name='stats'), 
-    path('sandbox/', views.view_sandbox, name='sandbox'),
+
     path('ajax/update_video_watch_time/', views.update_video_watch_time, name='updateWatchTime'),
     path('ajax/update_video_views/', views.update_video_views, name='updateViews'),
     path('ajax/update_video_rating/', views.update_video_rating, name='updateRating'),
